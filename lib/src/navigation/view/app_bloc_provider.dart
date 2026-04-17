adbe35175bff7d588c9e2f1ab2e767e2fa3cd2e1
Arthur <wangyn@qeek.cc>
2026-04-17 18:08:42 +0800

    格式化代码

--- lib/src/navigation/view/app_bloc_provider.dart
+++ lib/src/navigation/view/app_bloc_provider.dart
@@ -1,11 +1,11 @@
 import 'package:app/app.dart';
-import 'package:fx_updater/fx_updater.dart';
-import 'package:note/note.dart';
 import 'package:authentication/authentication.dart';
 import 'package:draw_system/draw_system.dart';
-import 'package:storage/storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fx_updater/fx_updater.dart';
+import 'package:note/note.dart';
+import 'package:storage/storage.dart';
 import 'package:widget_module/widget_module.dart';
 
 /// create by 张风捷特烈 on 2020/4/28
@@ -27,16 +27,12 @@ class _AppBlocProviderState extends State<AppBlocProvider> {
     return MultiBlocProvider(
       providers: [
         // 全局 bloc : 维护应用存储状态、更新、认证
-        BlocProvider<AuthBloc>(
-            create: (_) => AuthBloc(repository: HttpAuthRepository())),
+        BlocProvider<AuthBloc>(create: (_) => AuthBloc(repository: HttpAuthRepository())),
         BlocProvider<AppConfigBloc>(create: (_) => AppConfigBloc()),
-        BlocProvider<UpgradeBloc>(
-            create: (_) => UpgradeBloc(api: UnitUpgradeApi())),
+        BlocProvider<UpgradeBloc>(create: (_) => UpgradeBloc(api: UnitUpgradeApi())),
         BlocProvider<UserBloc>(create: (_) => UserBloc()),
         BlocProvider<NewsBloc>(create: (_) => NewsBloc()..initByCache()),
-
-        BlocProvider<GalleryUnitBloc>(
-            create: (_) => GalleryUnitBloc()..loadGalleryInfo()),
+        BlocProvider<GalleryUnitBloc>(create: (_) => GalleryUnitBloc()..loadGalleryInfo()),
       ],
       child: WidgetsBlocProvider(child: widget.child),
     );
